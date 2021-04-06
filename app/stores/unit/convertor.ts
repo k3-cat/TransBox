7d36c66a814aa8a25216ad90fa02185f2427245b
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2021-04-06 23:50:49 +0800

    improve conversion ux  & fix minor bugs

--- app/stores/unit/convertor.ts
+++ app/stores/unit/convertor.ts
@@ -1,20 +1,26 @@
 import { comMolIndex, vUnitIndex, wUnitIndex } from '../../screens/ho_units/utils';
 
+function getMol(mol: string) {
+  if (comMolIndex.has(mol)) {
+    return comMolIndex.get(mol)!;
+  }
+  const n = parseFloat(mol);
+  return n > 100 ? n : NaN;
+}
+
 export function convert(value: string, sUnit: string, tUnit: string, mol: string = '') {
-  var s = sUnit.split('/');
-  var t = tUnit.split('/');
+  const s = sUnit.split('/');
+  const t = tUnit.split('/');
 
-  var result = parseFloat(value) * wUnitIndex.get(s[0])! / wUnitIndex.get(t[0])! / vUnitIndex.get(s[1])! * vUnitIndex.get(t[1])!;
+  let result = parseFloat(value) * wUnitIndex.get(s[0])! / wUnitIndex.get(t[0])! / vUnitIndex.get(s[1])! * vUnitIndex.get(t[1])!;
 
-  var molN = (comMolIndex.has(mol) ? comMolIndex.get(mol)! : parseFloat(mol));
+  if (s[0].endsWith('mol')) { result *= getMol(mol); }
+  else if (s[0].endsWith('IU')) { result /= getMol(mol); }
 
-  if (s[0].endsWith('mol')) { result *= molN; }
-  else if (s[0].endsWith('IU')) { result /= molN; }
+  if (t[0].endsWith('mol')) { result /= getMol(mol); }
+  else if (t[0].endsWith('IU')) { result *= getMol(mol); }
 
-  if (t[0].endsWith('mol')) { result /= molN; }
-  else if (t[0].endsWith('IU')) { result *= molN; }
-
-  if (result > 10000 || (result != 0 && result < 0.001) || isNaN(result)) {
+  if (isNaN(result)) {
     return -1;
   }
   return result;
