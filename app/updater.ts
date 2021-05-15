0af92d7dd16bb9da183339c98db8f3997c72e081
Pix-00 <15878537+Pix-00@users.noreply.github.com>
2021-05-15 19:05:26 +1000

    style: remove custom header; fix: showing updater before select source

--- app/updater.ts
+++ app/updater.ts
@@ -33,7 +33,7 @@ function verComp(small: string, big: string) {
 }
 
 export async function triggerUpdate(R: IRootStore) {
-  if (R.settings.updatingSource === 'google') {
+  if (!R.settings.updatingSource || R.settings.updatingSource === 'google') {
     return;
   }
 
