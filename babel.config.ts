eceb15442e06e722cc1a5e6c22e7a6956bb91f63
k3-cat <k3-cat@outlook.com>
2022-06-05 17:12:48 +1000

    v1.2.2

--- babel.config.ts
+++ babel.config.ts
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    presets: ['babel-preset-expo',
+      ['env', { 'modules': false }],
+    ],
+    plugins: [
+      'react-native-paper/babel',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
